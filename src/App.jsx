9e2aaceabe064fd6b94800e18851292fdfce18e4
animesh28 <animesh.raj.om@gmail.com>
2023-07-20 12:52:45 +0530

    iPhone animation complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import DisplaySection from "./components/DisplaySection";
 import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
 import SoundSection from "./components/SoundSection";
+import WebGiViewer from "./components/WebGiViewer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Jumbotron />
       <SoundSection />
       <DisplaySection />
+      <WebGiViewer />
     </div>
   );
 }
